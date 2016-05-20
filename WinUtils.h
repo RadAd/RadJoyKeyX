abc8e579182d604abb02aabe46977cd7c91ee8f5
Adam Gates <adamgates84@gmail.com>
2016-05-20 13:20:38 +1000

    Support base in registry

--- WinUtils.h
+++ WinUtils.h
@@ -2,4 +2,5 @@
 
 DWORD GetModuleFileNameEx2(DWORD pid, LPWSTR lpFileName, DWORD nSize);
 HMODULE FindModule(DWORD pid, LPCWSTR lpModuleSpec);
+bool RegGetString(HKEY hKey, LPCWSTR lpValue, LPWSTR str, DWORD nLength, LPCWSTR def);
 void DebugOut(const TCHAR* format, ...);
